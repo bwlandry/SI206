cfbeb06d2b117f3b9dec4a808452dc2e34e75f76
Ben Landry <bwallacelandry34@yahoo.com>
2016-11-07 14:52:36 -0500

    Created a mad lib from Moby Dick text

--- HW3-StudentCopy/madlibhw3.py
+++ HW3-StudentCopy/madlibhw3.py
@@ -10,6 +10,52 @@
 # 1) Print the orginal text (150 tokens)
 # 1) Print the new text
 print("START*******")
+import nltk # requires some downloading/installing dependencies to use all its features; numpy is especially tricky to install
+import random
+from nltk import word_tokenize,sent_tokenize
 
 
+
+#print (nltk.corpus.gutenberg.fileids())   
+moby_dick = nltk.corpus.gutenberg.words("melville-moby_dick.txt")   #assign text to variable moby_dick
+#print (moby_dick)
+
+#print (moby_dick)
+# tokens = nltk.word_tokenize(moby_dick))
+
+
+tagged = nltk.pos_tag(moby_dick)[0:150]    #tagging each word with a part of speech. Results in tuples (word, POS)
+#print (tagged)
+
+
+
+tagmap = {"NN":"a noun","PRON":"a pronoun","VB":"a verb","JJ":"an adjective", "ADP": "an adposition (preposition)"}
+substitution_probabilities = {"NN":.15,"PRON":.1,"VB":.1,"JJ":.1, "ADP": .1}
+
+def spaced(word):
+	if word in [",", ".", "?", "!", ":"]:
+		return word
+	else:
+		return " " + word
+
+original_text = []
+for (word, tag) in tagged:
+	original_text.append(spaced(word))
+
+final_words = []
+
+
+for (word, tag) in tagged:
+	if tag not in substitution_probabilities or random.random() > substitution_probabilities[tag]:
+		final_words.append(spaced(word))
+	else:
+		new_word = input("Please enter %s:\n" % (tagmap[tag]))
+		final_words.append(spaced(new_word))
+
+
+print ("".join(original_text))
+print("\n")
+print("\n")
+print("\n")
+print ("".join(final_words))
 print("\n\nEND*******")
