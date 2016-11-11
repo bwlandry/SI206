bb6aa69468ae7a06805c65f9b7893d287a15efbc
Ben Landry <bwallacelandry34@yahoo.com>
2016-11-11 17:56:53 -0500

    adding bs file

--- HW3-StudentCopy/bshw3.py
+++ HW3-StudentCopy/bshw3.py
@@ -10,3 +10,50 @@
 
 # Deliverables
 # Make sure the new page is uploaded to your GitHub account.
+
+import requests
+from bs4 import BeautifulSoup
+import string
+import sys
+
+def uprint(*objects, sep=' ', end='\n', file=sys.stdout):
+
+    enc = file.encoding
+
+    if enc == 'UTF-8':
+
+        print(*objects, sep=sep, end=end, file=file)
+
+    else:
+
+        f = lambda obj: str(obj).encode(enc, errors='backslashreplace').decode(enc)
+
+        print(*map(f, objects), sep=sep, end=end, file=file)
+
+
+
+base_url = "http://collemc.people.si.umich.edu/data/bshw3StarterFile.html"
+r = requests.get(base_url)
+soup = BeautifulSoup(r.text, "html.parser")
+
+
+pretty = soup.prettify()
+
+
+new = pretty.replace("student", "AMAZING student")
+
+
+new = pretty.replace("https://testbed.files.wordpress.com/2012/09/bsi_exposition_041316_192.jpg", "/media/Headshot.jpg")
+#uprint(new)
+
+new = pretty.replace("logo2.png", "/media/logo.png")
+
+
+# images = soup('img')
+# uprint (images)
+
+
+f = open("bshw3_html.html", 'w')
+f.write(new)
+f.close
+
